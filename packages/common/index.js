f8bdff315b504f6f251952684715e0fabd0807c6
Tiziano <tizianodg@gmail.com>
2019-11-08 17:06:55 +0000

    Add styled components and common theme in web

---
+++ packages/common/index.js
@@ -0,0 +1,7 @@
+const theme = require("./theme");
+
+module.exports = {
+  theme,
+  a: message => console.log(`a: ${message}`),
+  b: message => console.log(`b: ${message}`)
+};
--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import a from 'a';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { theme } from "common";
+import styled, { ThemeProvider } from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Button = styled.button`
+  color: ${props => props.theme.mainColor};
+`;
 
 function App() {
-  a.test();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+          <Button>This is a button</Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
